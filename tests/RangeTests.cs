19fc567132dae3ffbae31deca4c82f61e5238a03
FARFETCH\mario.pereira <mario.pereira@farfetch.com>
2017-09-06 15:19:22 +0100

    added bug

--- tests/RangeTests.cs
+++ tests/RangeTests.cs
@@ -19,5 +19,16 @@ namespace tests
 
             Assert.Equal(expectedCount, generated.Count());
         }
+
+        [Fact]
+        public void SortShouldOrderResults()
+        {
+            var range = new Range {Count = 3, Sort = true } ;
+            var values = new[] {"a", "c", "b"};
+            var counter = 0;
+            var generated = range.Of(() => values[counter++]);
+
+            Assert.Equal(new[] {"a", "b", "c"}, generated.ToArray());
+        }
     }
 }
